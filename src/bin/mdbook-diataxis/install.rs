cbb8dbd9fc78aff04a2a2909d12d9be9d7c94a7d
kcza <ed@kcza.net>
2025-06-27 21:46:00 +0200

    Fix formatting

--- src/bin/mdbook-diataxis/install.rs
+++ src/bin/mdbook-diataxis/install.rs
@@ -1,7 +1,7 @@
 use std::fs;
 use std::path::{Path, PathBuf};
 
-use anyhow::{anyhow, Context, Result};
+use anyhow::{Context, Result, anyhow};
 use indoc::indoc;
 use toml_edit::{Array, DocumentMut, Item, Table};
 
